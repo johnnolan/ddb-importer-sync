9b554d4cfddf22c1877e53f835853e90c5122133
John Nolan <me@johnnolan.dev>
2021-09-30 08:49:15 +0100

    Adds events for inventory

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,30 @@ Hooks.once("init", async function () {
 });
 
 Hooks.on("updateActor", async function (data, diff) {
-  if (data.data.type === "character") {
-    console.log("DDB Importer Auto Sync | updateActor data", data);
-    console.log("DDB Importer Auto Sync | updateActor diff", diff);
-    const actor = game.actors.get(data.data._id);
-    if (diff.data?.attributes?.hp || diff.data?.spells) {
+  if (!game.user.isGM) return false;
+
+  if (data.data.type === "character") {    
+    if (diff.data?.attributes?.hp || diff.data?.spells || diff.data?.currency) {
       console.log("DDB Importer Auto Sync | updateActor Triggered");
-      DDBImporter.updateDDBCharacter(actor);
+      sync(data.data._id);
     } else {
       console.log("DDB Importer Auto Sync | updateActor Not Triggered");
     }
   }
 });
+
+Hooks.on("deleteItem", async function (data, diff, actorId) {
+  if (!game.user.isGM) return false;
+  sync(data.parent.data._id);
+});
+
+Hooks.on("createItem", async function (data, diff, actorId) {
+  if (!game.user.isGM) return false;
+  sync(data.parent.data._id);
+});
+
+function sync(actorId) {
+  console.log("DDB Importer Auto Sync | Syncing");
+  const actor = game.actors.get(actorId);
+  DDBImporter.updateDDBCharacter(actor);
+}
